f8de90af22480393a309a7a825897165fb09242c
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-24 21:52:25 +0300

    fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_141527) do
+ActiveRecord::Schema[7.0].define(version: 20_231_023_141_527) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "messages", force: :cascade do |t|
-    t.string "greeting"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'messages', force: :cascade do |t|
+    t.string 'greeting'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
